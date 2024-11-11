17fc9c0e4803524014f89991f08c81d4d2a93d32
egenerei <jor6edel123@gmail.com>
2024-11-12 00:11:35 +0100

    red: code eliminated in dni.php

--- formularios_php/TAREA1/dni.php
+++ formularios_php/TAREA1/dni.php
@@ -1,16 +1,15 @@
-
 <?php
 $dni = $_POST['dni'];
 function calculaLetraNIF($dni){
-    $result = 'EL DNI introducido no es correcto';
-    if (ctype_digit($dni) && strlen((string)$dni) == 8){
-        $resto_letras = [0 => "T", 1 => "R", 2 => "W", 3 => "A", 4 => "G", 5 => "M", 6 => "Y", 7 => "F",
-            8 => "P", 9 => "D", 10 => "X", 11 => "B", 12 => "N", 13 => "J", 14 => "Z",
-            15 => "S", 16 => "Q", 17 => "V", 18 => "H", 19 => "L", 20 => "C", 21 => "K",
-            22 => "E"];
-        $result = $resto_letras[$dni%23];
+    $mensaje_error = 'El DNI/NIF introducido no es correcto. Asegúrate de que continene 8 dígitos.';
+    if (ctype_digit($dni) && strlen($dni) == 8){
+        $resto_letras = [0 => 'T', 1 => 'R', 2 => 'W', 3 => 'A', 4 => 'G', 5 => 'M', 6 => 'Y', 7 => 'F',
+            8 => 'P', 9 => 'D', 10 => 'X', 11 => 'B', 12 => 'N', 13 => 'J', 14 => 'Z',
+            15 => 'S', 16 => 'Q', 17 => 'V', 18 => 'H', 19 => 'L', 20 => 'C', 21 => 'K',
+            22 => 'E'];
+        $mensaje_error = $resto_letras[$dni%23];
     }
-    return $result;
+    return $mensaje_error;
 }
 function verificadorNIF($dni){
     $validar = false;
@@ -25,17 +24,25 @@ function verificadorNIF($dni){
 
 #Programa (Dependiendo del número de cifras introudcidas, utiliza una función u otra)
 if (!empty($dni)) {
-    if (strlen($dni) == 8) { #Si solo se introducen 8 cifras se considera que se está buscando averiguar la letra del NIF
-        $letra = calculaLetraNIF($dni);
-        echo "La letra correspondiente al DNI es: " . $letra;
-    } elseif (strlen($dni) == 9) { #Si se introducen 9 caracteres, se busca validar el NIF
-        if (verificadorNIF($dni)) {
-            echo "El NIF es válido.";
-        } else {
-            echo "El NIF no es válido.";
+    if (ctype_digit($dni) && strlen($dni) == 8) { #Si solo se introducen 8 cifras se considera que se está buscando averiguar la letra del NIF
+    	$letra = calculaLetraNIF($dni);
+        if(strlen($letra)==1){
+        	echo 'La letra correspondiente al NIF '.$dni.' es: '. $letra;
         }
-    } else {
-        echo "El DNI/NIF introducido no es correcto.";
+        else {
+		    echo $letra;
+        }
+    } 
+    elseif (strlen($dni) == 9 && ctype_alnum($dni)) { #Si se introducen 9 caracteres, se busca validar el NIF
+        if (verificadorNIF($dni)) {
+            echo 'El NIF es válido.';
+        } 
+        else {
+            echo 'Error: el NIF no es válido. Verifica que la letra final sea correcta y que el número tenga 8 dígitos.';
+        }
+    } 
+    else {
+        echo 'Error: el formato introducido no es correcto. Introduce un número de 8 dígitos para calcular la letra o un NIF completo de 9 caracteres (8 dígitos y una letra).';
     }
 }
 if (empty($dni)){ #Por si no se introduce el DNI
