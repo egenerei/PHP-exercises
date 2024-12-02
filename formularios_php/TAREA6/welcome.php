dfe78fec34e00872b4f083fe568b484f99172258
egenerei <jor6edel123@gmail.com>
2024-12-02 16:34:58 +0100

    feat: new exercises

--- formularios_php/TAREA6/welcome.php
+++ formularios_php/TAREA6/welcome.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
+echo 'Bienvenido, '.$_SESSION['usuario'];
 if ($_SESSION['validar_inicio']==true){
-    echo 'Bienvenido, '.$_SESSION['usuario'];
+    echo '</br>';
+    echo 'Has iniciado sesión correctamente';
     echo '</br>';
     echo '<a href="logout.php">Logout</a>';
+}
+else{
+    echo 'No has iniciado sesión correctamente';
+    echo '<a href="logout.php">Vuelve a iniciar</a>'
 }
